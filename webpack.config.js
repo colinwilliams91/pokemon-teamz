9801868aedb8e907054469c32e7f88b52e1473c3
Miles Ager <miles.ager13@gmail.com>
2023-02-01 12:11:58 -0600

    minor correction

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,6 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
       }
     ]
   },
