0dab271716a347754c5a9510af4d0328140b7810
colin <colin.williams91@gmail.com>
2023-02-01 19:45:48 -0600

    combination merge styled jsx

--- client/src/components/Styled.jsx
+++ client/src/components/Styled.jsx
@@ -183,6 +183,35 @@ const TypeDiv = styled.div`
     }
 `;
 
+const blinkHover = keyframes`
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+`;
+
+const TypeDiv = styled.div`
+
+    width: 5rem;
+    height: 5rem;
+    margin: 5rem;
+    padding: 2rem;
+    letter-spacing: .2rem;
+    cursor: pointer;
+    text-align: center;
+    border-radius: 50%;
+    font-family: 'Pokemon Solid', sans-serif;
+    background: linear-gradient(to bottom, #ff0000, #ffff00);
+    &:hover{
+    animation: ${blinkHover} 1.5s ease-in-out infinite;
+    }
+`;
+
 export { Body, FoundUser, Header, InboxBody,
   UserMsg, UserInfo, Message, ListBody, Input,
   PokeEntry, Contents, CreateCharContainer,
